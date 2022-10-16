dcb60cf95e15f10417e3cdc783bba9f657913586
natali <v-natalii@mail.ru>
2022-10-16 10:11:44 +0300

    add task 01

---
+++ Task01/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число a ");
+int numa = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b ");
+int numb = Convert.ToInt32(Console.ReadLine());
+if (numb*numb == numa)
+{
+    Console.Write($"Число {numa} являеся вкадратом чила {numb}");
+}
+else 
+{
+    Console.Write($"Число {numa} не являеся вкадратом чила {numb}");
+}
+
