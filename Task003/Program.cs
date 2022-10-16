c7b24928106319eefe370880c65abdc8b514a5ed
natali <v-natalii@mail.ru>
2022-10-16 18:35:04 +0300

    add task 03

---
+++ Task003/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int count = (-num);
+
+while (count > (-num-1) && count < (num+1))
+{
+    Console.Write($"{count} ");
+    count++;
+}
