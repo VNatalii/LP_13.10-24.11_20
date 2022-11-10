931c182468ab4cdde6d31fd8323ddf813f05f29e
natali <v-natalii@mail.ru>
2022-11-10 16:50:15 +0300

    add task22

---
+++ Task22/Program.cs
@@ -0,0 +1,21 @@
+﻿
+void Table(int n)
+{
+    if (n>0)
+    {
+        int index = 1;
+        while (index <= n)
+        {
+            Console.WriteLine($"{index} {index*index}");
+            index++;
+        }
+    }
+    else Console.WriteLine("Некорректное число");
+}
+
+Console.WriteLine("Введите натуральное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Table(num);
+
+
