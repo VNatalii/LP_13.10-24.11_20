6d2b2771f35d55a4c575a68307ba7d47815b3ad2
natali <v-natalii@mail.ru>
2022-11-19 18:00:21 +0300

    add task28

---
+++ Task28/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+int Facorial(int num)
+{
+    int fact = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        fact = i*fact;
+    }
+    return fact;
+}
+
+Console.Write("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int factorial = Facorial(a);
+Console.WriteLine($"Произведение чисел от 1 до {a} равно {factorial}");
